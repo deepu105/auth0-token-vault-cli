756ab11741a5fdc33d662da77d52671ac09088cc
Deepu K Sasidharan <d4udts@gmail.com>
2026-03-28 23:13:33 +0100

    fix: CI timeout

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
     include: ['test/e2e/**/*.test.ts'],
     exclude: ['node_modules', 'dist'],
     setupFiles: ['test/setup.ts'],
+    testTimeout: 30_000,
+    hookTimeout: 30_000,
     typecheck: {
       tsconfig: './tsconfig.test.json',
     },
